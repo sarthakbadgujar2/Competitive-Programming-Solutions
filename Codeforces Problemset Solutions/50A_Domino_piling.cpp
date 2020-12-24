a9d2f938974ad1d5dcfae5e4ccf18b52d077bd1d
Sarthak <sarthakbadgujar2@gmail.com>
2020-12-24 09:40:08 +0530

    Problem : Domino piling

---
+++ Codeforces Problemset Solutions/50A_Domino_piling.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define	mod 1000000007
+typedef long long ll;
+#define endl "\n"
+
+
+
+int main()
+{
+	fastio;
+
+	int M,N;
+	cin >> M >> N;
+
+	cout << (M*N)/2;
+
+	
+}
+
+
